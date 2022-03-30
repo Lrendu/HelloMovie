d1ea02d566c9a8de9fa4194377982b46eb9ff1ab
lrendu <rendu.louise@gmail.com>
2022-03-30 18:44:39 +0200

    autorisations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :movie
+
+    validates :comment, presence :true
+    validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,6 @@
 class Playlist < ApplicationRecord
+  has_many :movies
+  belongs_to :users
+
+  validates :name, uniqueness: true, presence: true
 end
